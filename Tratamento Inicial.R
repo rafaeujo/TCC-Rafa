31413b1ed9a59e0d0da20059d5d02beed4d8298d
Alex Jordane <132111391+AlexJordane@users.noreply.github.com>
2026-05-31 15:01:35 -0300

    DataSet subido

--- Tratamento Inicial.R
+++ Tratamento Inicial.R
@@ -1,40 +1,31 @@
-install.packages("remotes")
-remotes::install_github("danicat/read.dbc")
-remotes::install_github("rfsaldanha/microdatasus")
+# 1. Instalação de pacotes (execute apenas se for necessário)
+# install.packages("remotes")
+# remotes::install_github("danicat/read.dbc")
+# remotes::install_github("rfsaldanha/microdatasus")
+
+# 2. Carregamento das bibliotecas
 library(microdatasus)
 library(dplyr)
+library(readr)
 
-dt2020brut <- fetch_datasus(
-  2020,
-  month_start = 1,
-  2020,
-  month_end = 1,
-  uf = "all",
-  "SIM-DO" ,
-  stop_on_error = FALSE,
-  timeout = 1000,
-  track_source = FALSE
-)
+# 3. Definição do período (agora incluindo 2019)
+anos <- 2019:2024
 
-dt2020p <- process_sim(data = dt2020)
-
-dt2020p <- dt2020p %>% 
-  filter(SEXO == "Feminino",
-      substr(CAUSABAS, 1, 3) >= "X85" &
-      substr(CAUSABAS, 1, 3) <= "Y09")
-
-  
-anos <- 2021:2025
-
+# 4. Inicialização das listas para armazenar os dados
 dt_brut <- list()
 dt_proc <- list()
 
-for(i in anos){
+# 5. Loop de consulta, processamento e filtragem
+for (ano in anos) {
   
-  dt_brut[[as.character(i)]] <- fetch_datasus(
-    year_start = i,
+  # Converte o ano para texto para usar como nome na lista
+  ano_str <- as.character(ano)
+  
+  # A. Download dos dados brutos (Nota: configurado apenas para janeiro)
+  dt_brut[[ano_str]] <- fetch_datasus(
+    year_start = ano,
     month_start = 1,
-    year_end = i,
+    year_end = ano,
     month_end = 1,
     uf = "all",
     information_system = "SIM-DO",
@@ -43,26 +34,19 @@ for(i in anos){
     track_source = FALSE
   )
   
-  dt_proc[[as.character(i)]] <- process_sim(
-    data = dt_brut[[as.character(i)]]
-  )
-}
-
-dt_brut <- c(list("2020" = dt2020), dt_brut)
-dt_proc <- c(list("2020" = dt2020p), dt_proc)
-
-dt2020p <- dt2020p %>% 
-  filter(SEXO == "Feminino",
-         substr(CAUSABAS, 1, 3) >= "X85" &
-           substr(CAUSABAS, 1, 3) <= "Y09")
-
-dt_proc <- lapply(dt_proc, function(x){
+  # B. Processamento dos dados
+  dt_processado <- process_sim(data = dt_brut[[ano_str]])
   
-  x %>%
+  # C. Filtragem (Feminino e Causa Básica X85 a Y09) e armazenamento na lista final
+  dt_proc[[ano_str]] <- dt_processado %>%
     filter(
       SEXO == "Feminino",
-      substr(CAUSABAS, 1, 3) >= "X85" &
-        substr(CAUSABAS, 1, 3) <= "Y09"
+      substr(CAUSABAS, 1, 3) >= "X85" & substr(CAUSABAS, 1, 3) <= "Y09"
     )
-  
-})
+}
+
+# Combina todos os elementos da lista em um único data frame
+dt_final <- bind_rows(dt_proc, .id = "ano_referencia")
+
+# Salva o dataset unificado em formato CSV
+write_csv(dt_final, "dados_sim_2019_2024.csv")
